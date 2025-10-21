d02b269e1f9e1c028ea536e1eadb22260e2f78b8
garnier-quentin <garnier.quentin@gmail.com>
2025-10-21 08:33:38 -0400

    wip

---
+++ src/cloud/openstack/restapi/mode/listloadbalancers.pm
@@ -0,0 +1,114 @@
+#
+# Copyright 2025 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package cloud::openstack::restapi::mode::listloadbalancers;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {});
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+}
+
+my @mapping = ('projectName', 'id', 'lbName', 'operatingStatus', 'provisioningStatus');
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $results = {};
+
+    my $projects = $options{custom}->get_projects();
+    foreach my $project (@$projects) {
+        my $lbs = $options{custom}->get_loadbalancers(project_id => $project->{id});
+        
+        foreach my $lb (@$lbs) {
+            $results->{ $project->{id} . $lb->{id} } = {
+                lbName => $lb->{name},
+                id => $lb->{id},
+                projectName => $project->{name},
+                operatingStatus => lc($lb->{operating_status}),
+                provisioningStatus => lc($lb->{provisioning_status})
+            };
+        }
+    }
+
+    return $results;
+}
+
+sub run {
+    my ($self, %options) = @_;
+
+    my $results = $self->manage_selection(custom => $options{custom});
+    foreach my $instance (sort keys %$results) {
+        $self->{output}->output_add(long_msg => 
+            join('', map("[$_: " . $results->{$instance}->{$_} . ']', @mapping))
+        );
+    }
+
+    $self->{output}->output_add(
+        severity => 'OK',
+        short_msg => 'List load balancers:'
+    );
+    $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1, force_long_output => 1);
+    $self->{output}->exit();
+}
+
+sub disco_format {
+    my ($self, %options) = @_;
+
+    $self->{output}->add_disco_format(elements => \@mapping);
+}
+
+sub disco_show {
+    my ($self, %options) = @_;
+
+    my $results = $self->manage_selection(custom => $options{custom});
+    foreach (sort keys %$results) {        
+        $self->{output}->add_disco_entry(
+            %{$results->{$_}}
+        );
+    }
+}
+1;
+
+__END__
+
+=head1 MODE
+
+List load balancers.
+
+=over 8
+
+=back
+
+=cut
--- src/cloud/openstack/restapi/plugin.pm
+++ src/cloud/openstack/restapi/plugin.pm
@@ -30,9 +30,10 @@ sub new {
     bless $self, $class;
 
     $self->{modes} = {
-        'cache'          => 'cloud::openstack::restapi::mode::cache',
-        'discovery'      => 'cloud::openstack::restapi::mode::discovery',
-        'load-balancers' => 'cloud::openstack::restapi::mode::loadbalancers'
+        'cache'               => 'cloud::openstack::restapi::mode::cache',
+        'discovery'           => 'cloud::openstack::restapi::mode::discovery',
+        'list-load-balancers' => 'cloud::openstack::restapi::mode::listloadbalancers',
+        'load-balancers'      => 'cloud::openstack::restapi::mode::loadbalancers'
     };
 
     $self->{custom_modes}->{api} = 'cloud::openstack::restapi::custom::api';
